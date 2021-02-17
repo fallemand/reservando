30899cebd9eb989334ee1160f4d678ba0ed1a7e4
Facundo Allemand <facundo.allemand@getyourguide.com>
2021-02-17 13:57:52 -0300

    Fix warnings vue-i18n

--- apps/frontend/build/webpack.config.dev.js
+++ apps/frontend/build/webpack.config.dev.js
@@ -11,6 +11,11 @@ module.exports = {
     ...webpackConfig.plugins,
     new ProgressBarPlugin(),
     new webpack.DefinePlugin({
+      __VUE_OPTIONS_API__: true,
+      __VUE_PROD_DEVTOOLS__: false,
+      __VUE_I18N_FULL_INSTALL__: false,
+      __VUE_I18N_LEGACY_API__: false,
+      __INTLIFY_PROD_DEVTOOLS__: false,
       GLOBAL: {
         // https://webpack.js.org/plugins/define-plugin/#feature-flags
         ENV: JSON.stringify(env),
