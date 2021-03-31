484afcf4fd1d076a8a1684b7254cdffbe202e49e
Facundo Allemand <facundo.allemand@getyourguide.com>
2021-03-31 19:29:10 +0200

    Move stories to components folder

--- apps/storybook/.storybook/main.js
+++ apps/storybook/.storybook/main.js
@@ -3,6 +3,7 @@ const path = require("path");
 module.exports = {
   stories: [
     "../stories/**/*.story.mdx",
+    "../node_modules/@reservando/design-system/**/*.story.mdx",
   ],
   addons: [
     "@storybook/addon-links",
@@ -20,6 +21,12 @@ module.exports = {
       test: /\.scss$/,
       use: ["vue-style-loader", "css-loader", "sass-loader"],
     });
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@storybook/addon-docs/blocks": path.resolve("./node_modules/@storybook/addon-docs/blocks"),
+      "@mdx-js/react": path.resolve("./node_modules/@mdx-js/react"),
+      "@reservando/design-system": path.resolve("./node_modules/@reservando/design-system"),
+    };
     return config;
   },
 };
